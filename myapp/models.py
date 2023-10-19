7e5726186825263664c8c1bbf79527bb706f4be3
Pankaj Padekar <padekarpankaj@gmail.com>
2023-10-19 21:54:40 +0530

    working

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    task_Title = models.CharField(max_length=50)
+    task_Desc = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task_Title
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render , HttpResponse
-
+from myapp.models import Task
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    context = {'success' : False}
+    if request.method == "POST":
+        #Handle the form
+        title = request.POST['title']
+        desc = request.POST['desc']
+        print(title , desc)
+        ins = Task(task_Title = title , task_Desc = desc)
+        ins.save()
+        context = {'success' : True}
+
+
+    return render(request, 'index.html' , context)
 
 def tasks(request):
     return render(request, 'tasks.html')
